cd3cf5372d01b7315e7270fd104c9dc2ef8f9739
opilane <opilane>
2024-11-06 12:25:40 +0200

    Write first test

--- SeleniumCsharp/FirstTestCase.cs
+++ SeleniumCsharp/FirstTestCase.cs
@@ -1,24 +1,59 @@
-﻿using OpenQA.Selenium.Firefox;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SeleniumCsharp
 {
     public class FirstTestCase
     {
-        static void Main(string[] args)
-        {
-            string binaryLocation = "I:\\Oppematerjal\\Tarkvara testimine\\FFP\\FirefoxPortable\\App\\Firefox64\\firefox.exe";
+        public string Dependencypath = "C:\\Users\\opilane\\source\\repos\\TARpe22\\Vider\\SeleniumCsharp\\SeleniumCsharp\\Dependencies";
+        private IWebDriver driver;
+        private WebDriverWait wait;
 
+        public static void Main() { }
+
+        [SetUp]
+        public void Setup()
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\TARpe22\\Vider\\FFP\\FirefoxPortable\\App\\Firefox64\\firefox.exe";
             FirefoxOptions options = new FirefoxOptions();
             options.BrowserExecutableLocation = binaryLocation;
 
-            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\TARpe22\\Vider\\SeleniumCsharp\\SeleniumCsharp\\drivers", options);
-            driver.Url = "https://store.steampowered.com/";
+            driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\TARpe22\\Vider\\SeleniumCsharp\\SeleniumCsharp\\drivers", options);
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(45));  
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Dispose();
+        }
+
+        [Test]
+        public void Test_progressBar()
+        {
+            driver.Url = "http://www.uitestingplayground.com/progressbar";
+
+            var startBtn = driver.FindElement(By.Id("startButton"));
+            startBtn.Click();
+
+            var progressBar = driver.FindElement(By.Id("progressBar"));
+
+            wait.Until(driver =>
+            {
+                var progress = Int32.Parse(progressBar.GetAttribute("aria-valuenow"));
+                return progress >= 70;
+            });
+
+            var stopBtn = driver.FindElement(By.Id("stopButton"));
+            stopBtn.Click();
+
+            var finalProgressValue = Int32.Parse(progressBar.GetAttribute("aria-valuenow"));
+
+            Assert.IsTrue(Math.Abs(finalProgressValue - 75) <= 5, $"Expected progress to be within 5% of 75, but got {finalProgressValue}%");
         }
     }
 }
